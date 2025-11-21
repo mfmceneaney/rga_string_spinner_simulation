0c203e2191a92196a18cab92f9d8728449bbbb66
Matthew F. McEneaney <mfmceneaney@gmail.com>
2025-11-21 15:03:47 -0500

    fix: Comment out unecessary slurm option.

--- jobs/simulation/mc_rga/submit.sh
+++ jobs/simulation/mc_rga/submit.sh
@@ -7,7 +7,7 @@
 #SBATCH --account=clas12
 #SBATCH -c 1
 #SBATCH --mem-per-cpu=2000
-#SBATCH --gres=disk:5000
+##SBATCH --gres=disk:5000
 #SBATCH --time=24:00:00
 
 $SSS_HOME/jobs/simulation/mc_rga/job.sh
